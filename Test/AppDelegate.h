07fcf4d990f5b43a9f1600f67070e96fdaf78141
desjason <des_jason@163.com>
2012-11-16 00:21:14 +0800

    1 commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Lu Cao on 12-11-16.
+//  Copyright (c) 2012年 Lu Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestTests/TestTests.h
@@ -0,0 +1,13 @@
+//
+//  TestTests.h
+//  TestTests
+//
+//  Created by Lu Cao on 12-11-16.
+//  Copyright (c) 2012年 Lu Cao. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestTests : SenTestCase
+
+@end
